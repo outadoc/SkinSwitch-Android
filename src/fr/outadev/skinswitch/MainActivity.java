3e44a96d105f0a502d98afeae8bbda94140fd187
outadoc <outadoc@gmail.com>
2014-03-09 15:52:15 +0100

    We can now check if the minecraft.net credentials are correct

--- src/fr/outadev/skinswitch/MainActivity.java
+++ src/fr/outadev/skinswitch/MainActivity.java
@@ -1,8 +1,11 @@
 package fr.outadev.skinswitch;
 
+import fr.outadev.skinswitch.network.NetworkHandler;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
 
@@ -10,6 +13,20 @@ public class MainActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		new AsyncTask<Void, Void, Boolean>() {
+			@Override
+			protected Boolean doInBackground(Void... params) {
+				NetworkHandler networkHandler = new NetworkHandler(MainActivity.this);
+				return Boolean.valueOf(networkHandler.checkMojangCredentials("testUser", "testPassword"));
+			}
+
+			@Override
+			protected void onPostExecute(Boolean result) {
+				Toast.makeText(MainActivity.this, result.toString(), Toast.LENGTH_LONG).show();
+			}
+
+		}.execute();
 	}
 
 	@Override
