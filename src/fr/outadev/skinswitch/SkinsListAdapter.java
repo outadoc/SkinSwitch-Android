e6f95bcd9bc4315a7df48e83838196f65b763b27
outadoc <outadoc@gmail.com>
2014-06-27 23:15:23 +0200

    Async loading of the head previews \o/

--- src/fr/outadev/skinswitch/SkinsListAdapter.java
+++ src/fr/outadev/skinswitch/SkinsListAdapter.java
@@ -4,6 +4,8 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,18 +27,33 @@ public class SkinsListAdapter extends ArrayAdapter<Skin> {
 			convertView = inflater.inflate(R.layout.skin_icon, parent, false);
 		}
 
-		ImageView skinView = (ImageView) convertView.findViewById(R.id.img_skin_preview);
+		final ImageView skinView = (ImageView) convertView.findViewById(R.id.img_skin_preview);
 		TextView skinTitle = (TextView) convertView.findViewById(R.id.lbl_skin_title);
 
-		Skin skin = getItem(position);
-
+		final Skin skin = getItem(position);
 		skinTitle.setText(skin.getName());
-		
-		try {
-	        skinView.setImageBitmap(skin.getSkinHeadBitmap(getContext()));
-        } catch(FileNotFoundException e) {
-	        skinView.setImageResource(R.drawable.broken_image);
-        }
+
+		(new AsyncTask<Void, Void, Bitmap>() {
+
+			@Override
+			protected Bitmap doInBackground(Void... params) {
+				try {
+					return skin.getSkinHeadBitmap(getContext());
+				} catch(FileNotFoundException e) {
+					return null;
+				}
+			}
+
+			@Override
+			protected void onPostExecute(Bitmap result) {
+				if(result != null) {
+					skinView.setImageBitmap(result);
+				} else {
+					skinView.setImageResource(R.drawable.broken_image);
+				}
+			}
+
+		}).execute();
 
 		return convertView;
 	}
