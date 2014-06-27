5c0550ca5eb8f2069fd21ec65ef56ceda0b3008f
outadoc <outadoc@gmail.com>
2014-06-27 20:41:17 +0200

    Improve scrolling performance

--- src/fr/outadev/skinswitch/SkinsListAdapter.java
+++ src/fr/outadev/skinswitch/SkinsListAdapter.java
@@ -2,7 +2,6 @@ package fr.outadev.skinswitch;
 
 import java.util.List;
 
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -18,31 +17,31 @@ import fr.outadev.skinswitch.skin.SkinRenderer.Side;
 
 public class SkinsListAdapter extends ArrayAdapter<Skin> {
 
-	public SkinsListAdapter(Activity activity, int resource, List<Skin> array) {
-		super(activity, resource, array);
-		this.activity = activity;
+	public SkinsListAdapter(Context context, int resource, List<Skin> array) {
+		super(context, resource, array);
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View cell = inflater.inflate(R.layout.skin_icon, parent, false);
+		LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		
+		if(convertView == null) {
+			convertView =  inflater.inflate(R.layout.skin_icon, parent, false);
+		}
 
-		ImageView skinView = (ImageView) cell.findViewById(R.id.img_skin_preview);
-		TextView skinTitle = (TextView) cell.findViewById(R.id.lbl_skin_title);
+		ImageView skinView = (ImageView) convertView.findViewById(R.id.img_skin_preview);
+		TextView skinTitle = (TextView) convertView.findViewById(R.id.lbl_skin_title);
 
 		BitmapFactory.Options opt = new BitmapFactory.Options();
 		opt.inScaled = false;
 
 		skinTitle.setText(getItem(position).getName());
 
-		Bitmap btmp = BitmapFactory.decodeResource(activity.getResources(), R.drawable.test_skin_outadoc, opt);
+		Bitmap btmp = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.test_skin_outadoc, opt);
 		skinView.setImageBitmap(SkinRenderer.getCroppedHead(SkinRenderer.getSkinPreview(btmp, Side.FRONT, 19)));
 		btmp.recycle();
 
-		return cell;
+		return convertView;
 	}
-
-	private final Activity activity;
 
 }
