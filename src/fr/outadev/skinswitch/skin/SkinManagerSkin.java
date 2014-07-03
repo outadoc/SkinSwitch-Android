76f1eaa6f4456be5e36159acf05b376f4985cc9c
outadoc <outadoc@gmail.com>
2014-07-03 23:46:09 +0200

    Override method in SkinManagerSkin to get the right bitmap

--- src/fr/outadev/skinswitch/skin/SkinManagerSkin.java
+++ src/fr/outadev/skinswitch/skin/SkinManagerSkin.java
@@ -1,6 +1,11 @@
 package fr.outadev.skinswitch.skin;
 
+import java.io.FileNotFoundException;
 import java.util.Date;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import fr.outadev.skinswitch.network.skinmanager.SkinManagerConnectionHandler;
 
 public class SkinManagerSkin extends Skin {
 
@@ -17,6 +22,18 @@ public class SkinManagerSkin extends Skin {
 
 	public void setOwner(String owner) {
 		this.owner = owner;
+	}
+	
+	@Override
+	public Bitmap getRawSkinBitmap(Context context) throws FileNotFoundException {
+	    SkinManagerConnectionHandler handler = new SkinManagerConnectionHandler();
+	    Bitmap bmp = handler.fetchSkinBitmap(getId());
+	    
+	    if(bmp == null) {
+	    	throw new FileNotFoundException("Couldn't fetch skin for id=" + getId());
+	    }
+	    
+	    return bmp;
 	}
 
 }
