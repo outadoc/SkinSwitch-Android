3791ea5b883d3601049804ff4d19d5b2acd52a6d
outadoc <outadoc@gmail.com>
2014-07-04 13:01:24 +0200

    Little cleanup

--- src/fr/outadev/skinswitch/MainActivity.java
+++ src/fr/outadev/skinswitch/MainActivity.java
@@ -1,12 +1,8 @@
 package fr.outadev.skinswitch;
 
-import java.io.FileNotFoundException;
-
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import fr.outadev.skinswitch.network.skinmanager.SkinManagerConnectionHandler;
 import fr.outadev.skinswitch.user.UsersManager;
 
 public class MainActivity extends FragmentActivity {
@@ -24,27 +20,6 @@ public class MainActivity extends FragmentActivity {
 				startActivity(intent);
 			}
 		}
-
-		(new AsyncTask<Void, Void, Void>() {
-
-			@Override
-			protected Void doInBackground(Void... params) {
-				SkinManagerConnectionHandler handler = new SkinManagerConnectionHandler();
-
-				System.out.println(handler.fetchLatestSkins());
-				System.out.println(handler.fetchRandomSkins());
-				try {
-	                System.out.println(handler.fetchSkinByName("outadoc").get(0).getSkinHeadBitmap(MainActivity.this));
-                } catch(FileNotFoundException e) {
-	                // TODO Auto-generated catch block
-	                e.printStackTrace();
-                }
-				System.out.println(handler.fetchSkinBitmap(10));
-
-				return null;
-			}
-
-		}).execute();
 	}
 
 	@Override
--- src/fr/outadev/skinswitch/NewCustomSkinActivity.java
+++ src/fr/outadev/skinswitch/NewCustomSkinActivity.java
@@ -8,6 +8,7 @@ public class NewCustomSkinActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_add_custom_skin);
 	}
 
