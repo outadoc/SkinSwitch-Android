600f99037e738ff5e155f581b30f7a0fb554a130
outadoc <outadoc@gmail.com>
2014-07-05 17:25:38 +0200

    Show skin info and preview in library

--- app/src/main/java/fr/outadev/skinswitch/adapters/SkinLibraryListAdapter.java
+++ app/src/main/java/fr/outadev/skinswitch/adapters/SkinLibraryListAdapter.java
@@ -1,11 +1,16 @@
 package fr.outadev.skinswitch.adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 import fr.outadev.skinswitch.R;
@@ -15,15 +20,43 @@ public class SkinLibraryListAdapter extends ArrayAdapter<SkinLibrarySkin> {
 
 	public SkinLibraryListAdapter(Context context, int resource, List<SkinLibrarySkin> objects) {
 		super(context, resource, objects);
-
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
+	public View getView(final int position, View convertView, ViewGroup parent) {
 		if(convertView == null) {
 			LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			convertView = inflater.inflate(R.layout.skin_library_card, parent, false);
 		}
+
+		TextView txt_skin_name = (TextView) convertView.findViewById(R.id.lbl_skin_name);
+		TextView txt_skin_description = (TextView) convertView.findViewById(R.id.lbl_skin_description);
+		final ImageView img_skin_preview = (ImageView) convertView.findViewById(R.id.img_skin_preview);
+
+		txt_skin_name.setText(getItem(position).getName());
+		txt_skin_description.setText(getItem(position).getDescription());
+		img_skin_preview.setImageResource(R.drawable.char_front); //loading
+
+		(new AsyncTask<Void, Void, Bitmap>() {
+
+			@Override
+			protected Bitmap doInBackground(Void... voids) {
+				try {
+					return getItem(position).getFrontSkinPreview(getContext());
+				} catch(FileNotFoundException e) {
+
+				}
+
+				return null;
+			}
+
+			@Override
+			protected void onPostExecute(Bitmap bitmap) {
+				if(bitmap != null) {
+					img_skin_preview.setImageBitmap(bitmap);
+				}
+			}
+		}).execute();
 
 		return convertView;
 	}
