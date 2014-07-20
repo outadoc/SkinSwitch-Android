14c5b90cbee29bf6b9979019fb3708802112a051
outadoc <outadoc@gmail.com>
2014-07-20 15:45:23 +0200

    Add basic settings

--- app/src/main/java/fr/outadev/skinswitch/settings/SettingsActivity.java
+++ app/src/main/java/fr/outadev/skinswitch/settings/SettingsActivity.java
@@ -2,18 +2,34 @@ package fr.outadev.skinswitch.settings;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 /**
  * Created by outadoc on 19/07/14.
  */
 public class SettingsActivity extends Activity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		if(getActionBar() != null) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
 
 		// Display the fragment as the main content.
 		getFragmentManager().beginTransaction()
 				.replace(android.R.id.content, new SettingsFragment())
 				.commit();
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+			case android.R.id.home:
+				this.finish();
+				return true;
+		}
+
+		return false;
 	}
 }
--- app/src/main/java/fr/outadev/skinswitch/settings/SettingsFragment.java
+++ app/src/main/java/fr/outadev/skinswitch/settings/SettingsFragment.java
@@ -1,6 +1,8 @@
 package fr.outadev.skinswitch.settings;
 
 import android.annotation.SuppressLint;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
@@ -10,6 +12,7 @@ import fr.outadev.skinswitch.R;
  * Created by outadoc on 19/07/14.
  */
 public class SettingsFragment extends PreferenceFragment {
+
 	@SuppressLint("Override")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -18,4 +21,18 @@ public class SettingsFragment extends PreferenceFragment {
 		// Load the preferences from an XML resource
 		addPreferencesFromResource(R.xml.prefs);
 	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		try {
+			PackageInfo info = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
+			findPreference("pref_about").setSummary(getResources().getString(R.string.about_skinswitch_sum,
+					getResources().getString(R.string.app_name), info.versionName));
+		} catch(PackageManager.NameNotFoundException e1) {
+			e1.printStackTrace();
+		}
+	}
+
 }
