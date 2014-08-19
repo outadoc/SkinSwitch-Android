4bf4f4052ada037e22854574a0c33d28f9793145
outadoc <outadoc@gmail.com>
2014-08-19 18:58:42 +0200

    Add GPLv3 headers

--- app/src/main/java/fr/outadev/skinswitch/user/User.java
+++ app/src/main/java/fr/outadev/skinswitch/user/User.java
@@ -1,3 +1,21 @@
+/*
+ * SkinSwitch - User
+ * Copyright (C) 2014-2014  Baptiste Candellier
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package fr.outadev.skinswitch.user;
 
 /**
@@ -19,16 +37,16 @@ public class User {
 		return password;
 	}
 
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public String getUsername() {
 		return username;
 	}
 
 	public void setUsername(String username) {
 		this.username = username;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 
 }
